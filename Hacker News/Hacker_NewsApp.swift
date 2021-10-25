248e61c27dfc07a3e53e99558f4b8f04988f3e74
Sima Nerush <2002ssn@gmail.com>
2021-10-25 15:38:36 -0700

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Serafima on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
